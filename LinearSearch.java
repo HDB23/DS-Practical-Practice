8350f7eee88a76a688f316fca6d5e7bcc20209c6
HDB23 <hetbordiwala2006@gmail.com>
2025-11-10 15:45:11 +0530

    Updated with try catch

--- LinearSearch.java
+++ LinearSearch.java
@@ -6,24 +6,32 @@ public class LinearSearch
         Scanner sc = new Scanner(System.in);
         int customerID[] = {1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1009, 1010}; 
         System.out.print("Enter a key : ");
-        int key = sc.nextInt();
-        boolean flag = false;
-
-        for(int i=0; i<customerID.length; i++)
+        try
         {
-            if(key == customerID[i])
+            int key = sc.nextInt();
+            boolean flag = false;
+
+            for(int i=0; i<customerID.length; i++)
             {
-                flag = true;
+                if(key == customerID[i])
+                {
+                    flag = true;
+                }
+            }
+            if(flag == true)
+            {
+                System.out.println("Found");
+            }
+            else
+            {
+                System.out.println("Not Found");
             }
         }
-        if(flag == true)
+        catch(Exception e)
         {
-            System.out.println("Found");
+            System.out.println("Please enter integer value !");
         }
-        else
-        {
-            System.out.println("Not Found");
-        }
+
         sc.close();
     }
 }
