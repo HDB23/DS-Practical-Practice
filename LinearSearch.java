31dd7953e048c1faf46c7e2ca6db09e3a4d6b635
HDB23 <hetbordiwala2006@gmail.com>
2025-11-10 15:36:27 +0530

    LinearSearch Added

---
+++ LinearSearch.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class LinearSearch
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int customerID[] = {1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1009, 1010}; 
+        System.out.print("Enter a key : ");
+        int key = sc.nextInt();
+        boolean flag = false;
+
+        for(int i=0; i<customerID.length; i++)
+        {
+            if(key == customerID[i])
+            {
+                flag = true;
+            }
+        }
+        if(flag == true)
+        {
+            System.out.println("Found");
+        }
+        else
+        {
+            System.out.println("Not Found");
+        }
+        sc.close();
+    }
+}
